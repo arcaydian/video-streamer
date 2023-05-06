658576facebac1b290b1921ccc42b2e7f89d4a9f
arcaydian <130350819+arcaydian@users.noreply.github.com>
2023-05-06 17:17:13 +0530

    Refactored StreamService.java

--- streamer/src/main/java/com/video/service/StreamService.java
+++ streamer/src/main/java/com/video/service/StreamService.java
@@ -17,77 +17,76 @@ import jakarta.annotation.PostConstruct;
 
 @Service
 public class StreamService {
-	
-	private final String CONTENT_TYPE = "Content-Type";
-	private final String CONTENT_LENGTH = "Content-Length";
-	private final String VIDEO_CONTENT = "video/mp4";
-	private final String CONTENT_RANGE = "Content-Range";
-	private final String ACCEPT_RANGES = "Accept-Ranges";
-	private final String BYTES = "bytes";
-	private final int CHUNK_SIZE = 10000;	
-	
-	private static final Logger logger = (Logger) LoggerFactory.getLogger(StreamService.class);
-	
-	private String staticFilesPath;
 
-	
-    public ResponseEntity<byte[]> streamVideoInChunks(final String fileName, final String range) {
-	    try {
-		    long rangeStart = 0;
-		    long rangeEnd = CHUNK_SIZE;
-		    final Long fileSize = getFileSize(fileName);
-		    
-		    if (range == null || range.equals("")) {
-		        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-		                .header(CONTENT_TYPE, VIDEO_CONTENT)
-		                .header(ACCEPT_RANGES, BYTES)
-		                .header(CONTENT_LENGTH, String.valueOf(rangeEnd))
-		                .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
-		                .header(CONTENT_LENGTH, String.valueOf(fileSize))
-		                .body(readByteRange(fileName, rangeStart, rangeEnd));
-		    }
-		    
-		    String[] ranges = range.split("-");
-		    
-		    rangeStart = Long.parseLong(ranges[0].substring(6));
-		    
-		    if (ranges.length > 1) {
-		        rangeEnd = Long.parseLong(ranges[1]);
-		    } else {
-		        rangeEnd = rangeStart + CHUNK_SIZE;
-		    }
-				    
-		    if(rangeEnd > fileSize - 1) {
-		    	rangeEnd = fileSize - 1;
-		    }
-		    
-		    final byte[] data = readByteRange(fileName, rangeStart, rangeEnd);
-		    final String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
-		    
-		    HttpStatus httpStatus = HttpStatus.PARTIAL_CONTENT;
-		    
-		    if (rangeEnd >= fileSize) {
-		        httpStatus = HttpStatus.OK;
-		    }
-		    
-		    return ResponseEntity.status(httpStatus)
-		            .header(CONTENT_TYPE, VIDEO_CONTENT)
-		            .header(ACCEPT_RANGES, BYTES)
-		            .header(CONTENT_LENGTH, contentLength)
-		            .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
-		            .body(data);
-	    } 
-	    catch (IOException e) {
-	        logger.error("Exception while reading the file {}", e.getMessage());
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	    }
+    private final String CONTENT_TYPE = "Content-Type";
+    private final String CONTENT_LENGTH = "Content-Length";
+    private final String VIDEO_CONTENT = "video/mp4";
+    private final String CONTENT_RANGE = "Content-Range";
+    private final String ACCEPT_RANGES = "Accept-Ranges";
+    private final String BYTES = "bytes";
+    private final int CHUNK_SIZE = 10000;
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(StreamService.class);
+
+    private String staticFilesPath;
+
+
+    public ResponseEntity < byte[] > streamVideoInChunks(final String fileName, final String range) {
+        try {
+            long rangeStart = 0;
+            long rangeEnd = CHUNK_SIZE;
+            final Long fileSize = getFileSize(fileName);
+
+            if (range == null || range.equals("")) {
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                    .header(CONTENT_TYPE, VIDEO_CONTENT)
+                    .header(ACCEPT_RANGES, BYTES)
+                    .header(CONTENT_LENGTH, String.valueOf(rangeEnd))
+                    .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+                    .header(CONTENT_LENGTH, String.valueOf(fileSize))
+                    .body(readByteRange(fileName, rangeStart, rangeEnd));
+            }
+
+            String[] ranges = range.split("-");
+
+            rangeStart = Long.parseLong(ranges[0].substring(6));
+
+            if (ranges.length > 1) {
+                rangeEnd = Long.parseLong(ranges[1]);
+            } else {
+                rangeEnd = rangeStart + CHUNK_SIZE;
+            }
+
+            if (rangeEnd > fileSize - 1) {
+                rangeEnd = fileSize - 1;
+            }
+
+            final byte[] data = readByteRange(fileName, rangeStart, rangeEnd);
+            final String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+
+            HttpStatus httpStatus = HttpStatus.PARTIAL_CONTENT;
+
+            if (rangeEnd >= fileSize) {
+                httpStatus = HttpStatus.OK;
+            }
+
+            return ResponseEntity.status(httpStatus)
+                .header(CONTENT_TYPE, VIDEO_CONTENT)
+                .header(ACCEPT_RANGES, BYTES)
+                .header(CONTENT_LENGTH, contentLength)
+                .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+                .body(data);
+        } catch (IOException e) {
+            logger.error("Exception while reading the file {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     private byte[] readByteRange(String filename, long start, long end) throws IOException {
         Path path = Paths.get(getFilePath(filename));
         byte[] data = Files.readAllBytes(path);
-        byte[] result = new byte[(int) (end - start) + 1];
-        System.arraycopy(data, (int) start, result, 0, (int) (end - start) + 1);
+        byte[] result = new byte[(int)(end - start) + 1];
+        System.arraycopy(data, (int) start, result, 0, (int)(end - start) + 1);
         return result;
     }
 
@@ -97,9 +96,9 @@ public class StreamService {
 
     private Long getFileSize(String fileName) {
         return Optional.ofNullable(fileName)
-                .map(file -> Paths.get(getFilePath(file)))
-                .map(this::sizeFromFile)
-                .orElse(0L);
+            .map(file - > Paths.get(getFilePath(file)))
+            .map(this::sizeFromFile)
+            .orElse(0 L);
     }
 
     private Long sizeFromFile(Path path) {
@@ -108,12 +107,12 @@ public class StreamService {
         } catch (IOException ioException) {
             logger.error("Error while getting the file size", ioException);
         }
-        return 0L;
+        return 0 L;
     }
-    
+
     @PostConstruct
     void init() {
-    	File pwdFile = new File("");
-    	staticFilesPath = pwdFile.getAbsolutePath() + "/src/main/resources/static/";
+        File pwdFile = new File("");
+        staticFilesPath = pwdFile.getAbsolutePath() + "/src/main/resources/static/";
     }
 }
